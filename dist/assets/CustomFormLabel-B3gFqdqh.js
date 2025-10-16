bc770c09bea777603282b56af1c0ded9236bf329
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2025-10-16 17:44:26 +0530

    backend api route changed

--- dist/assets/CustomFormLabel-B3gFqdqh.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CmVnqgw7.js";import{s as m,T as a}from"./Typography-C1gRltuG.js";const i=m(o=>t.jsx(a,{variant:"subtitle1",fontWeight:600,...o,component:"label",htmlFor:o.htmlFor}))(()=>({marginBottom:"5px",marginTop:"25px",display:"block"}));export{i as C};
--- dist/assets/useThemeProps-BiBoc5Nu.js
+++
@@ -1,1 +0,0 @@
-import{M as m,T as o}from"./index-CmVnqgw7.js";import{u as s}from"./useThemeProps-Cfiozmo_.js";function a({props:e,name:r}){return s({props:e,name:r,defaultTheme:m,themeId:o})}export{a as u};
