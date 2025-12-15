a5e010913891708682ca194d974d85a896b1fb8f
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2025-12-15 15:10:45 +0530

    added the backend url to the .env file

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api/v1/',
-  // baseURL: 'http://localhost:3000/api/v1/',
+  // baseURL: '/api/v1/',
+  baseURL: import.meta.env.VITE_BASE_BACKEND_URL,
   headers: {
     'Content-Type': 'multipart/form-data'
   },
