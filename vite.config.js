618d91cb9bbd55d123f3c323b94a64c7ef18fbe6
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2025-12-16 11:10:36 +0530

    vite-congig-updated

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,8 @@ export default defineConfig({
         {
           name: 'load-js-files-as-jsx',
           setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+            // Fixed: use forward slash for cross-platform compatibility
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
               loader: 'jsx',
               contents: await fs.readFile(args.path, 'utf8'),
             }));
@@ -36,13 +37,5 @@ export default defineConfig({
     },
   },
 
-  // plugins: [react(),svgr({
-  //   exportAsDefault: true
-  // })],
-
   plugins: [svgr(), react()],
 });
-
-
-// Image Width	174
-// Image Height	26
