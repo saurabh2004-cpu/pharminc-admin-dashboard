646209f704e527b505746fb336416c2a124d84ef
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2025-12-16 10:38:03 +0530

    vite-congig-updated

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
-import svgr from '@svgr/rollup'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs/promises';
+import svgr from '@svgr/rollup';
 
-// ESM-safe __dirname replacement
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       src: resolve(__dirname, 'src'),
     },
   },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
 
-  plugins: [
-    react(),
-    svgr({ exportAsDefault: true }),
-  ],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+              loader: 'jsx',
+              contents: await fs.readFile(args.path, 'utf8'),
+            }));
+          },
+        },
+      ],
+    },
+  },
+
+  // plugins: [react(),svgr({
+  //   exportAsDefault: true
+  // })],
+
+  plugins: [svgr(), react()],
+});
+
+
+// Image Width	174
+// Image Height	26
