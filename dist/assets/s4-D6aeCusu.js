6996532bcafab6052f9b74b5659f0db81edc324e
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2025-12-18 12:27:57 +0530

    removed default api calls

---
+++ dist/assets/s4-D6aeCusu.js
@@ -0,0 +1,1 @@
+const s="/assets/s6-BHYP62n9.jpg",a="/assets/s9-Oqnmwd8d.jpg",t="/assets/s7-CTzWdjv0.jpg",c="/assets/s4-DZcRYgYm.jpg";export{s as a,t as b,a as c,c as s};
---
+++ dist/assets/useThemeProps-Csz4yiIh.js
@@ -0,0 +1,1 @@
+import{F as m,T as o}from"./index-BNfkBZw4.js";import{u as s}from"./useThemeProps-klCC8Lc4.js";function a({props:e,name:r}){return s({props:e,name:r,defaultTheme:m,themeId:o})}export{a as u};
