46fedcdcf944c7d0c356db8ffe65d13c3dd7f096
saurabhbuzzlinkstudios-glitch <saurabhbuzzlinkstudios@gmail.com>
2026-07-13 17:13:58 +0530

    .

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   // for production uncomment this line
   // baseURL: '/api/v1/',
 
-  //for the local setup
+  // for the local setup ///////
   baseURL: import.meta.env.VITE_BASE_BACKEND_URL,
 
   headers: {},
